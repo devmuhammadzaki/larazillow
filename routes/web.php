dc35543a15ad3bc9e0e59b1f8f8003e1169b7939
Muhammad Zaki <mahmadzekoo@gmail.com>
2024-03-09 00:39:40 +0200

    auth middleware

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,13 @@ use App\Http\Controllers\ListingController;
 */
 
 Route::get('/', [IndexController::class, 'index']);
-Route::get('/show', [IndexController::class, 'show']);
+Route::get('/show', [IndexController::class, 'show'])->middleware('auth');
 
-Route::resource('listing', ListingController::class);
+Route::resource('listing', ListingController::class)
+    ->only(['create', 'store', 'edit', 'update', 'destroy'])
+    ->middleware('auth');
+Route::resource('listing', ListingController::class)
+    ->except(['create', 'store', 'edit', 'update', 'destroy']);
 
 Route::get('login', [AuthController::class, 'create'])->name('login');
 Route::post('login', [AuthController::class, 'store'])->name('login.store');
