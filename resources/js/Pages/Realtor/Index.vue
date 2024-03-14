6c2b02484f76f30aeb6efc23d73ed4cbe2cf7511
Muhammad Zaki <mahmadzekoo@gmail.com>
2024-03-14 03:54:23 +0200

    linting

--- resources/js/Pages/Realtor/Index.vue
+++ resources/js/Pages/Realtor/Index.vue
@@ -14,14 +14,20 @@ defineProps({
 </script>
 
 <template>
-    <h1 class="text-3xl mb-4">Your Listings</h1>
+    <h1 class="text-3xl mb-4">
+        Your Listings
+    </h1>
 
     <section>
         <RealtorFilters :filters="filters" />
     </section>
 
     <section class="grid grid-cols-1 lg:grid-cols-2 gap-2">
-        <Box v-for="listing in listings.data" :key="listing.id" :class="{ 'border-dashed': listing.deleted_at }">
+        <Box
+            v-for="listing in listings.data"
+            :key="listing.id"
+            :class="{ 'border-dashed': listing.deleted_at }"
+        >
             <div class="flex flex-col md:flex-row gap-2 md:items-center justify-between">
                 <div :class="{ 'opacity-25': listing.deleted_at }">
                     <div class="xl:flex items-center gap-2">
@@ -32,19 +38,36 @@ defineProps({
                     <ListingAddress :listing="listing" />
                 </div>
                 <div class="flex items-center gap-1 text-gray-600 dark:text-gray-300">
-                    <a class="btn-outline text-xs font-medium" :href="route('listing.show', { listing: listing.id })"
-                        target="_blank">Preview</a>
-                    <Link class="btn-outline text-xs font-medium"
-                        :href="route('realtor.listing.edit', { listing: listing.id })">Edit</Link>
-
-                    <Link v-if="!listing.deleted_at" class="btn-outline text-xs font-medium"
-                        :href="route('realtor.listing.destroy', { listing: listing.id })" as="button" method="delete">
-                    Delete
+                    <a
+                        class="btn-outline text-xs font-medium"
+                        :href="route('listing.show', { listing: listing.id })"
+                        target="_blank"
+                    >Preview</a>
+                    <Link
+                        class="btn-outline text-xs font-medium"
+                        :href="route('realtor.listing.edit', { listing: listing.id })"
+                    >
+                        Edit
                     </Link>
 
-                    <Link v-else class="btn-outline text-xs font-medium"
-                        :href="route('realtor.listing.restore', { listing: listing.id })" as="button" method="put">
-                    Restore
+                    <Link
+                        v-if="!listing.deleted_at"
+                        class="btn-outline text-xs font-medium"
+                        :href="route('realtor.listing.destroy', { listing: listing.id })"
+                        as="button"
+                        method="delete"
+                    >
+                        Delete
+                    </Link>
+
+                    <Link
+                        v-else
+                        class="btn-outline text-xs font-medium"
+                        :href="route('realtor.listing.restore', { listing: listing.id })"
+                        as="button"
+                        method="put"
+                    >
+                        Restore
                     </Link>
                 </div>
             </div>
