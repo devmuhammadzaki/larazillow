2e6095d018043ebd355631d982c7bf3576f5e930
Muhammad Zaki <mahmadzekoo@gmail.com>
2024-03-14 20:32:34 +0200

    upload backend frontend

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -9,15 +9,21 @@ const flashSuccess = computed(() => page.props.value.flash.success);
 const user = computed(
     () => page.props.value.user,
 );
+
+const prenav = window.location.origin;
 </script>
 
 <template>
     <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 w-full">
         <div class="container mx-auto">
             <nav class="p-4 flex items-center justify-between">
-                <div class="text-lg font-medium">
+                <div>
                     <Link :href="route('listing.index')">
-                        Listings
+                        <img
+                            width="40"
+                            height="40"
+                            :src="prenav + '/storage/larazillowlogo.png'"
+                        >
                     </Link>
                 </div>
                 <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
---
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -0,0 +1,50 @@
+<script setup>
+import Box from '@/Components/UI/Box.vue';
+import { useForm } from '@inertiajs/inertia-vue3';
+
+const props = defineProps({ listing: Object });
+
+const form = useForm({
+    images: [],
+});
+const upload = () => {
+    form.post(
+        route('realtor.listing.image.store', { listing: props.listing.id }),
+        {
+            onSuccess: () => form.reset('images'),
+        },
+    );
+};
+const addFiles = (event) => {
+    for (const image of event.target.files) {
+        form.images.push(image);
+    }
+};
+const reset = () => form.reset('images');
+</script>
+
+<template>
+    <Box>
+        <template #header>
+            Upload New Images
+        </template>
+        <form @submit.prevent="upload">
+            <input
+                type="file"
+                multiple
+                class="mr-5"
+                @input="addFiles"
+            >
+            <button type="submit" class="btn-outline mr-2">
+                Upload
+            </button>
+            <button
+                type="reset"
+                class="btn-outline"
+                @click="reset"
+            >
+                Reset
+            </button>
+        </form>
+    </Box>
+</template>
